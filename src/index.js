4376e71a16fdae90fcf2924e20c5d388b73f2258
CVJaswanthiReddy <jaswanthireddy95@gmail.com>
2024-05-23 13:23:08 +0530

    SIgnup logic with password encryption

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 
+const apiRoutes= require('./routes/index');
 const { PORT }= require('./config/serverConfig');
+const bodyParser = require('body-parser');
 const app = express();
-
+// const{User}= require('./models/index');
+// const bcrypt=require('bcrypt');
 const prepareAndStartServer=() =>{
 
-    app.listen(PORT,() => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',apiRoutes);
+
+    app.listen(PORT,async() => {
         console.log(`server started on Port: ${PORT}`);
+        // const incomingpassword='12345';
+        // const user=await User.findByPk(2);
+        // const response=bcrypt.compareSync(incomingpassword,user.password);
+        // console.log(response);
     })
 }
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require('bcrypt');
+const { SALT } = require('../config/serverConfig');
+const { use } = require('../routes');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -32,6 +35,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+  });
+
+  User.beforeCreate((user) =>{
+    const encryptedpassword= bcrypt.hashSync(user.password,SALT);
+    user.password=encryptedpassword;
   });
   return User;
 };
