825c601f22279cb39e377ab7532b2f798e1f77bf
CVJaswanthiReddy <jaswanthireddy95@gmail.com>
2024-05-24 11:20:19 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const app = express();
 // const{User}= require('./models/index');
 // const bcrypt=require('bcrypt');
+
+//const UserRepository= require('./repository/user-repository');
 const prepareAndStartServer=() =>{
 
 
@@ -16,6 +18,9 @@ const prepareAndStartServer=() =>{
 
     app.listen(PORT,async() => {
         console.log(`server started on Port: ${PORT}`);
+        // const repo= new UserRepository();
+        // const response=await repo.getById(1);
+        // console.log(response);
         // const incomingpassword='12345';
         // const user=await User.findByPk(2);
         // const response=bcrypt.compareSync(incomingpassword,user.password);
